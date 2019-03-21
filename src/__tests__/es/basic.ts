d426f6929c48be3a11500babf0f40c33aec865d8
MasuqaT <occar@hotmail.co.jp>
2019-03-21 17:02:30 +0900

    feat: when no comments in a file

--- src/__tests__/es/basic.ts
+++ src/__tests__/es/basic.ts
@@ -1,6 +1,7 @@
 import { prelude, generateTestCode, fragments } from "../test-utils";
 
-beforeAll(prelude(["Jest"]));
+// Using all test frameworks
+beforeAll(prelude(["Jest", "MagicComment"]));
 
 describe("babel-plugin-remove-test-code for es, basic tests", () => {
   it("transforms nothing if test code does not exists", () => {
--- src/magic-comment-process.ts
+++ src/magic-comment-process.ts
@@ -1,7 +1,23 @@
 import { NodePath } from "@babel/traverse";
-import { collectDeclaredVariablesShallow } from "./utils";
 import * as babel from "@babel/core";
+// @ts-ignore
+import { BaseComment, File } from "@babel/types";
+
+// Original Syntax
+// need to be a block comment like eslint
+// /* test-code-start ~~ */
+// /* test-code-end ~~ */
+// (~~ is optional string)
+// If "end" does not appear after "start", this process removes all lines after "start"
 
 export default function(t: typeof babel.types, programPath: NodePath): void {
+  const fileNode = programPath.parent as File;
+  const comments = fileNode.comments as BaseComment[];
+
+  // no comments
+  if (comments.length === 0) {
+    return;
+  }
+
   throw Error("Not Implemented");
 }
